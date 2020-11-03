1d6f2e85d655fa8c02f6fa86f846ac7a3e5a4d86
hugojose144 <hugo.perez@zabud.com.co>
2020-11-03 14:00:06 -0500

    Agregando json y configuración para un evento 2 en jms

--- src/main/java/org/mutualser/sprinboot/app/configuration/JMSRouteBuilder.java
+++ src/main/java/org/mutualser/sprinboot/app/configuration/JMSRouteBuilder.java
@@ -4,8 +4,12 @@ import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.mutualser.sprinboot.app.commons.AssemblerPost;
 import org.mutualser.sprinboot.app.domain.dto.PersonDto;
+import org.mutualser.sprinboot.app.domain.dto.Staff;
 import org.mutualser.sprinboot.app.domain.model.Person;
 import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 
 @Component("jmsConfig")
@@ -13,24 +17,39 @@ public class JMSRouteBuilder extends RouteBuilder {
  
   private Person person;
   
+  private Staff staff;
+  
   private Gson gson; 
   
-  public JMSRouteBuilder(Gson gson) {
-    this.gson = gson;
-  }
+  ObjectMapper mapper;
   
+  public JMSRouteBuilder(Gson gson,ObjectMapper mapper) {
+    this.gson = gson;
+    this.mapper = mapper;
+  }
   
   @Override
   public void configure() throws Exception {
     
- // Envia un mensaje cada 1 segundo
-    from("timer:mytimer?period=5000")
+ // Envia un mensaje cada x segundos
+    from("timer:mytimer?period=30000")
             .to("direct:firstRoute");
+    
+    from("timer:mytimer?period=3000")
+    .to("direct:secondRoute");
+    
     
  // Recibe un mensaje en la cola
     from("direct:firstRoute")
     //.setBody(constant("Hello"))
     .process(this::createEvent)
+    .log("Queue -> ${body}")
+    .to("jms:queue:hugoPrueba");
+   
+ // Recibe un mensaje en la cola
+    from("direct:secondRoute")
+    //.setBody(constant("Hello"))
+    .process(this::createEventTwo)
     .log("Queue -> ${body}")
     .to("jms:queue:hugoPrueba");
     
@@ -45,6 +64,22 @@ public class JMSRouteBuilder extends RouteBuilder {
     String personString = gson.toJson(personDto);
 
     exchange.getIn().setBody(personString);
+    
+  }
+  
+  public void createEventTwo(Exchange exchange) throws JsonMappingException, JsonProcessingException {
+    
+    mapper = new ObjectMapper();
+
+    String json = "[{ \"name\" : \"Hugo\", \"lastName\" : \"Pérez\" }]";    
+
+    Object objectJson = mapper.readValue(json, Object.class); 
+    
+    staff = Staff.builder().id(1).content(objectJson).build();
+    
+    String staffJson = gson.toJson(staff);
+    
+    exchange.getIn().setBody(staffJson);
     
   }
   
